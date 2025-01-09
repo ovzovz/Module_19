46cd35e35c36f7ad4655b3f4cc5cc8fc66a15462
ovzovz <ovz.ovz@ya.ru>
2025-01-09 23:29:13 +0300

    Add files via upload

---
+++ task1/main.py
@@ -0,0 +1,24 @@
+import os
+###os.system('python -m venv venv')
+#os.system('source .venv/bin/activate')
+#os.system('pip install django')
+#os.system('python.exe -m pip install --upgrade pip')
+#os.system('django-admin startproject UrbanDjango')
+#os.chdir('UrbanDjango')
+#print(os.getcwd())
+#os.system('python manage.py startapp task1')
+#os.system("pip freeze > requirements.txt")
+
+#os.chdir('UrbanDjango')
+#os.system("python manage.py migrate")
+#os.system('python manage.py makemigrations')
+#os.system("python manage.py migrate")
+
+
+"""
+if __name__=='__main__':
+    os.chdir('UrbanDjango')
+    os.system('python manage.py runserver')
+
+"""
+
