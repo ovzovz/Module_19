6cacd2075e3db490229eee7b7f19682aa0bdd883
ovzovz <ovz.ovz@ya.ru>
2025-01-11 20:18:14 +0300

    Add files via upload

---
+++ task2/UrbanDjango/task1/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+class Buyer(models.Model):
+    name = models.CharField(max_length=30)
+    balance = models.DecimalField(max_digits=8, decimal_places=2)
+    age = models.IntegerField()
+
+class Game(models.Model):
+    title = models.CharField(max_length=100)
+    cost = models.DecimalField(max_digits=8, decimal_places=2)
+    size = models.DecimalField(max_digits=8, decimal_places=2)
+    description = models.TextField()
+    age_limited = models.BooleanField(default=False)
+
+    buyer = models.ManyToManyField(Buyer, related_name='games' )
+
+    def __str__(self):
+        return self.title
